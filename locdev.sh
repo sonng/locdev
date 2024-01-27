43a0a347dd0e73f246fdcfe283b58199bd28dd6e
Son <anh.s.nguyen@gmail.com>
2024-01-27 18:30:50 -0500

    Finally! Booted up and ready to go

--- locdev.sh
+++ locdev.sh
@@ -8,27 +8,29 @@ function install_dependencies() {
 
 function prepare_disk() {
     # Copy Image Disk
-    cp $FEDORA_IMAGE_LOCATION /tmp/fedora_image.qcow2
+    cp $VM_IMAGE_LOCATION /tmp/vm_image.qcow2
 
     # Convert & Resize Image Disk
-    qemu-img convert -f qcow2 -O raw /tmp/fedora_image.qcow2 /tmp/fedora_image.raw
+    qemu-img convert -p -f qcow2 -O raw /tmp/vm_image.qcow2 /tmp/vm_image.raw
 }
 
 function prepare_cloud_init() {
     # Create Cloud Image
-    echo "local-hostname: $LOCDEV_HOSTNAME" > cloud-init/metadata
+    echo "local-hostname: $LOCDEV_HOSTNAME" > cloud-init/meta-data
 
     # replace $USER in user-data with the current user
     sed "s%\$USER%$LOCDEV_USERNAME%g; s%\$PASSWORD%$LOCDEV_PASSWORD%g; s%\$SSH_KEY%$LOCDEV_SSH_KEY%g" templates/user-data.template > cloud-init/user-data
 
     mkisofs -output cloud-init.iso -volid cidata -joliet -rock cloud-init/
+
+    cp cloud-init.iso /tmp/cloud-init.iso
 }
 
 function create_vm() {
     # Create VM
     tart delete "$LOCDEV_VM_NAME" || true
     tart create --linux "$LOCDEV_VM_NAME"
-    mv /tmp/fedora_image.raw ~/.tart/vms/$LOCDEV_VM_NAME/disk.img
+    mv /tmp/vm_image.raw ~/.tart/vms/$LOCDEV_VM_NAME/disk.img
 }
 
 function pack_image() {
@@ -45,9 +47,11 @@ function build() {
 }
 
 function clean() {
-    rm -rf /tmp/fedora_image.raw
+    rm -rf /tmp/vm_image.raw
+    rm -rf /tmp/cloud-init.iso
     rm -rf cloud-init.iso
     rm -rf cloud-init/user-data
+    rm -rf cloud-init/meta-data
     
     tart delete "$LOCDEV_VM_NAME" || true
 }
